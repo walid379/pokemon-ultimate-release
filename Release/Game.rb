fb21dca296705f8ab9aa4db53a6dc80cd1cdd04f
walid379 <belhoutwalid13@gmail.com>
2024-10-30 00:06:50 +0100

    Release 30/10/2024

--- Release/Game.rb
+++ Release/Game.rb
@@ -1,6 +1,1 @@
 RubyVM::InstructionSequence.load_from_binary(File.binread('Game.yarb')).eval
-begin
-  $GAME_LOOP.call
-rescue Exception
-  display_game_exception('An error occured during Game Loop.')
-end
